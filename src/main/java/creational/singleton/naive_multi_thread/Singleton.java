fb834f317b183651dd836ab0b60fa34afa75b25b
Yusuf Turan <yusuftrn@rocketmail.com>
2021-01-20 11:49:57 +0300

    singleton

--- src/main/java/creational/singleton/naive_multi_thread/Singleton.java
+++ src/main/java/creational/singleton/naive_multi_thread/Singleton.java
@@ -1,23 +1,22 @@
-package creational.singleton.naive_single_thread;
+package creational.singleton.naive_multi_thread;
 
 public final class Singleton {
 
     private static Singleton instance;
     public String value;
 
-    private Singleton(String value){
+    private Singleton(String value) {
         // The following code emulates slow initialization.
-        try{
+        try {
             Thread.sleep(1000);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
         }
         this.value = value;
     }
 
-    public static Singleton getInstance(String value){
-        if(instance == null){
+    public static Singleton getInstance(String value) {
+        if (instance == null) {
             instance = new Singleton(value);
         }
         return instance;
